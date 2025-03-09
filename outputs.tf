c9e257ee5634fea51fbe9eb063f22a3b414c53b4
David Thuilot <dthuilot@hotmail.com>
2025-03-09 17:22:59 -0500

    Fix outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -17,11 +17,6 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_id" {
-  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
-  value       = module.eks.cluster_id
-}
-
 output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
@@ -30,16 +25,6 @@ output "cluster_name" {
 output "cluster_oidc_issuer_url" {
   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
   value       = module.eks.cluster_oidc_issuer_url
-}
-
-output "cluster_platform_version" {
-  description = "Platform version for the cluster"
-  value       = module.eks.cluster_platform_version
-}
-
-output "cluster_status" {
-  description = "Status of the EKS cluster. One of CREATING, ACTIVE, DELETING, FAILED"
-  value       = module.eks.cluster_status
 }
 
 output "cluster_version" {
@@ -51,23 +36,14 @@ output "cluster_version" {
 # Security Group
 ################################################################################
 
-output "cluster_security_group_arn" {
-  description = "Amazon Resource Name (ARN) of the cluster security group"
-  value       = module.eks.cluster_security_group_arn
+output "cluster_primary_security_group_id" {
+  description = "Cluster security group that was created by Amazon EKS for the cluster. Managed node groups use this security group for control-plane-to-data-plane communication"
+  value       = module.eks.cluster_primary_security_group_id
 }
 
 output "cluster_security_group_id" {
   description = "ID of the cluster security group"
   value       = module.eks.cluster_security_group_id
-}
-
-################################################################################
-# Node Security Group
-################################################################################
-
-output "node_security_group_arn" {
-  description = "Amazon Resource Name (ARN) of the node shared security group"
-  value       = module.eks.node_security_group_arn
 }
 
 output "node_security_group_id" {
@@ -76,36 +52,17 @@ output "node_security_group_id" {
 }
 
 ################################################################################
-# IRSA
-################################################################################
-
-output "oidc_provider" {
-  description = "The OpenID Connect identity provider (issuer URL without leading https://)"
-  value       = module.eks.oidc_provider
-}
-
-output "oidc_provider_arn" {
-  description = "The ARN of the OIDC Provider if enable_irsa = true"
-  value       = module.eks.oidc_provider_arn
-}
-
-################################################################################
 # IAM Role
 ################################################################################
-
-output "cluster_iam_role_name" {
-  description = "IAM role name of the EKS cluster"
-  value       = module.eks.cluster_iam_role_name
-}
 
 output "cluster_iam_role_arn" {
   description = "IAM role ARN of the EKS cluster"
   value       = module.eks.cluster_iam_role_arn
 }
 
-output "cluster_iam_role_unique_id" {
-  description = "Stable and unique string identifying the IAM role"
-  value       = module.eks.cluster_iam_role_unique_id
+output "cluster_iam_role_name" {
+  description = "IAM role name of the EKS cluster"
+  value       = module.eks.cluster_iam_role_name
 }
 
 ################################################################################
@@ -116,17 +73,3 @@ output "eks_managed_node_groups" {
   description = "Map of attribute maps for all EKS managed node groups created"
   value       = module.eks.eks_managed_node_groups
 }
-
-output "eks_managed_node_groups_autoscaling_group_names" {
-  description = "List of the autoscaling group names created by EKS managed node groups"
-  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
-}
-
-################################################################################
-# Additional
-################################################################################
-
-output "aws_auth_configmap_yaml" {
-  description = "Formatted yaml output for base aws-auth configmap containing roles used in cluster node groups/fargate profiles"
-  value       = module.eks.aws_auth_configmap_yaml
-} 
